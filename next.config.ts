a6314ccbb1e7f47ed36278571c92ce8795211260
SuperWindCloud <19338823723@163.com>
2025-12-29 15:24:49 +0800

    chore: bump version

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  reactCompiler :true 
 };
 
 export default nextConfig;
